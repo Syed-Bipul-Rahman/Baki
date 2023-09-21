a1e119c6c0751d1949052ba8b2abec27c78615d5
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-09-22 04:02:30 +0600

    Splash Screen added

--- app/src/main/java/com/graphforge/bakiapps/SplashScreen.java
+++ app/src/main/java/com/graphforge/bakiapps/SplashScreen.java
@@ -2,7 +2,9 @@ package com.graphforge.bakiapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        //sent main activity after 3 second
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, 3000);
+
     }
 }
