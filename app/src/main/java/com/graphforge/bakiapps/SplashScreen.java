29545d1fee35861c74b3bd7355784d7e5fd155cc
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-09-26 21:48:51 +0600

    navigation drawer added

--- app/src/main/java/com/graphforge/bakiapps/SplashScreen.java
+++ app/src/main/java/com/graphforge/bakiapps/SplashScreen.java
@@ -18,7 +18,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent home = new Intent(SplashScreen.this, MainActivity.class);
+                Intent home = new Intent(SplashScreen.this, HomePage.class);
                 startActivity(home);
                 finish();
             }
